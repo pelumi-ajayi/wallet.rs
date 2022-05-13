e78f78133d7718776dc2e3a1478ac2a844444c59
Tuditi <daviddetroch@pm.me>
2022-05-13 13:47:49 +0200

    chore: remove redundant types

--- bindings/nodejs/types/bridge/account.ts
+++ bindings/nodejs/types/bridge/account.ts
@@ -10,14 +10,6 @@ export type __SyncAccountPayloadMethod__ = {
     data?: AccountSyncOptions
 }
 
-export type __SyncAccountPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SyncAccountPayloadMethod__
-    };
-}
-
 export type __GetInfoPayloadMethod__ = {
     name: 'GetNodeInfo',
     data: string[]
@@ -30,36 +22,12 @@ export type __GenerateAddressesPayloadMethod__ = {
     }
 }
 
-export type __GenerateAddressesPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __GenerateAddressesPayloadMethod__
-    };
-}
-
 export type __LatestAddressPayloadMethod__ = {
     name: 'GetLatestAddress'
 }
 
-export type __LatestAddressPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __LatestAddressPayloadMethod__
-    };
-}
-
 export type __BalancePayloadMethod__ = {
     name: 'GetBalance'
-}
-
-export type __BalancePayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __BalancePayloadMethod__
-    };
 }
 
 export type __SetClientOptionsPayload__ = {
@@ -74,14 +42,6 @@ export type __SetCollectOutputsPayloadMethod__ = {
     }
 }
 
-export type __SetCollectOutputsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SetCollectOutputsPayloadMethod__
-    };
-}
-
 export type __GetOutputsWithAdditionalUnlockConditionsPayloadMethod__ = {
     name: 'GetOutputsWithAdditionalUnlockConditions',
     data: {
@@ -89,48 +49,16 @@ export type __GetOutputsWithAdditionalUnlockConditionsPayloadMethod__ = {
     }
 }
 
-export type __GetOutputsWithAdditionalUnlockConditionsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __GetOutputsWithAdditionalUnlockConditionsPayloadMethod__
-    };
-}
-
 export type __ListAddressesPayloadMethod__ = {
     name: 'ListAddresses'
-}
-
-export type __ListAddressesPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __ListAddressesPayloadMethod__
-    };
 }
 
 export type __ListAddressesWithBalancePayloadMethod__ = {
     name: 'ListAddressesWithBalance'
 }
 
-export type __ListAddressesWithBalancePayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __ListAddressesWithBalancePayloadMethod__
-    };
-}
-
 export type __ListOutputsPayloadMethod__ = {
     name: 'ListOutputs'
-}
-
-export type __ListOutputsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __ListOutputsPayloadMethod__
-    };
 }
 
 export type __ListPendingTransactionsPayloadMethod__ = {
@@ -149,24 +77,8 @@ export type __ListTransactionsPayloadMethod__ = {
     name: 'ListTransactions'
 }
 
-export type __ListTransactionsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __ListTransactionsPayloadMethod__
-    };
-}
-
 export type __ListUnspentOutputsPayloadMethod__ = {
     name: 'ListUnspentOutputs'
-}
-
-export type __ListUnspentOutputsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __ListUnspentOutputsPayloadMethod__
-    };
 }
 
 export type __MintNativeTokenPayloadMethod__ = {
@@ -177,28 +89,12 @@ export type __MintNativeTokenPayloadMethod__ = {
     }
 }
 
-export type __MintNativeTokenPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __MintNativeTokenPayloadMethod__
-    };
-}
-
 export type __MintNftsPayloadMethod__ = {
     name: 'MintNfts',
     data: {
         nftsOptions: NftOptions;
         options: TransferOptions
     }
-}
-
-export type __MintNftsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __MintNftsPayloadMethod__
-    };
 }
 
 export type __SendAmountPayloadMethod__ = {
@@ -209,28 +105,12 @@ export type __SendAmountPayloadMethod__ = {
     }
 }
 
-export type __SendAmountPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SendAmountPayloadMethod__
-    };
-}
-
 export type __SendMicroTransactionPayloadMethod__ = {
     name: 'SendMicroTransaction',
     data: {
         addressesWithMicroAmount: AddressWithMicroAmount[];
         options: TransferOptions
     }
-}
-
-export type __SendMicroTransactionPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SendMicroTransactionPayloadMethod__
-    };
 }
 
 export type __SendNativeTokensPayloadMethod__ = {
@@ -241,28 +121,12 @@ export type __SendNativeTokensPayloadMethod__ = {
     }
 }
 
-export type __SendNativeTokensPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SendNativeTokensPayloadMethod__
-    };
-}
-
 export type __SendNftPayloadMethod__ = {
     name: 'SendNft',
     data: {
         addressesNftIds: AddressNftId[];
         options: TransferOptions
     }
-}
-
-export type __SendNftPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SendNftPayloadMethod__
-    };
 }
 
 export type __SendTransferPayloadMethod__ = {
@@ -273,25 +137,9 @@ export type __SendTransferPayloadMethod__ = {
     }
 }
 
-export type __SendTransferPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __SendTransferPayloadMethod__
-    };
-}
-
 export type __TryCollectOutputsPayloadMethod__ = {
     name: 'TryCollectOutputs',
     data: {
         outputsToCollect: OutputsToCollect;
     }
-}
-
-export type __TryCollectOutputsPayload__ = {
-    cmd: 'CallAccountMethod'
-    payload: {
-        accountId: AccountId;
-        method: __TryCollectOutputsPayloadMethod__
-    };
 }
--- bindings/nodejs/types/bridge/index.ts
+++ bindings/nodejs/types/bridge/index.ts
@@ -22,27 +22,8 @@ import type {
     __SendNftPayloadMethod__,
     __SendTransferPayloadMethod__,
     __TryCollectOutputsPayloadMethod__,
-    __SyncAccountPayload__,
-    __GenerateAddressesPayload__,
-    __LatestAddressPayload__,
-    __BalancePayload__,
     __SetClientOptionsPayload__,
-    __SetCollectOutputsPayload__,
-    __GetOutputsWithAdditionalUnlockConditionsPayload__,
-    __ListAddressesPayload__,
-    __ListAddressesWithBalancePayload__,
-    __ListOutputsPayload__,
     __ListPendingTransactionsPayload__,
-    __ListTransactionsPayload__,
-    __ListUnspentOutputsPayload__,
-    __MintNativeTokenPayload__,
-    __MintNftsPayload__,
-    __SendAmountPayload__,
-    __SendMicroTransactionPayload__,
-    __SendNativeTokensPayload__,
-    __SendNftPayload__,
-    __SendTransferPayload__,
-    __TryCollectOutputsPayload__
 } from './account';
 import type {
     __GetAccountsMessagePayload__,
@@ -56,23 +37,23 @@ import type {
     __GenerateMnemonicPayload__,
     __VerifyMnemonicPayload__,
     __DeleteStorageMessagePayload__,
+    __StartBackgroundSyncPayload__,
+    __StopBackgroundSyncPayload__,
     
 } from './accountManager'
 
 export type __AccountPayloadMethods__ =
-    | __SyncAccountPayloadMethod__
-    | __GetInfoPayloadMethod__
-    | __GenerateAddressesPayloadMethod__
-    | __LatestAddressPayloadMethod__
     | __BalancePayloadMethod__
-    | __SetCollectOutputsPayloadMethod__ 
+    | __GenerateAddressesPayloadMethod__
+    | __GetInfoPayloadMethod__
     | __GetOutputsWithAdditionalUnlockConditionsPayloadMethod__
+    | __LatestAddressPayloadMethod__
     | __ListAddressesPayloadMethod__
     | __ListAddressesWithBalancePayloadMethod__
     | __ListOutputsPayloadMethod__
-    | __ListUnspentOutputsPayloadMethod__
     | __ListPendingTransactionsPayloadMethod__ 
     | __ListTransactionsPayloadMethod__
+    | __ListUnspentOutputsPayloadMethod__
     | __MintNativeTokenPayloadMethod__
     | __MintNftsPayloadMethod__
     | __SendAmountPayloadMethod__
@@ -80,6 +61,8 @@ export type __AccountPayloadMethods__ =
     | __SendNativeTokensPayloadMethod__
     | __SendNftPayloadMethod__
     | __SendTransferPayloadMethod__
+    | __SetCollectOutputsPayloadMethod__ 
+    | __SyncAccountPayloadMethod__
     | __TryCollectOutputsPayloadMethod__
 
 export type __CallAccountMethodPayload__ = {
@@ -91,36 +74,19 @@ export type __CallAccountMethodPayload__ = {
 }
 
 export type __SendMessagePayload__ =
-    | __GetAccountsMessagePayload__
-    | __GetAccountMessagePayload__
+    | __BackupPayload__
+    | __CallAccountMethodPayload__
     | __CreateAccountMessagePayload__
     | __DeleteStorageMessagePayload__
-    | __CallAccountMethodPayload__
-    | __SetStrongholdPasswordPayload__
     | __GenerateMnemonicPayload__
-    | __StoreMnemonicPayload__
-    | __VerifyMnemonicPayload__
-    | __BackupPayload__
-    | __RestoreBackupPayload__
-    | __SyncAccountPayload__
+    | __GetAccountMessagePayload__
+    | __GetAccountsMessagePayload__
     | __GetNodeInfoPayload__
-    | __GenerateAddressesPayload__
-    | __LatestAddressPayload__
-    | __BalancePayload__
-    | __SetClientOptionsPayload__
-    | __SetCollectOutputsPayload__
-    | __GetOutputsWithAdditionalUnlockConditionsPayload__
-    | __ListAddressesPayload__
-    | __ListAddressesWithBalancePayload__
-    | __ListOutputsPayload__
     | __ListPendingTransactionsPayload__
-    | __ListTransactionsPayload__
-    | __ListUnspentOutputsPayload__
-    | __MintNativeTokenPayload__
-    | __MintNftsPayload__
-    | __SendAmountPayload__
-    | __SendMicroTransactionPayload__
-    | __SendNativeTokensPayload__
-    | __SendNftPayload__
-    | __SendTransferPayload__
-    | __TryCollectOutputsPayload__;
+    | __RestoreBackupPayload__
+    | __StartBackgroundSyncPayload__
+    | __StopBackgroundSyncPayload__
+    | __SetClientOptionsPayload__
+    | __SetStrongholdPasswordPayload__
+    | __StoreMnemonicPayload__
+    | __VerifyMnemonicPayload__;
